db659e6f2b2ab0c4d1391f12d0ed256c89ff2c30
Luke <62490330+Luke20201@users.noreply.github.com>
2025-04-23 17:27:00 +0100

    Create tsudio_frogcafe.lua

---
+++ server/locations/tsudio_frogcafe.lua
@@ -0,0 +1,101 @@
+--https://www.gta5-mods.com/maps/mlo-burgershot-2023-add-on-sp-fivem
+local jobloc = 'frogperson'
+
+Jobs['frogperson'] = {
+    CateringEnabled = false,
+    closedShopsEnabled = true,
+    Blip = {
+        --{sprite = 106, color = 2, scale = 0.5, label = 'Burger Shot', loc = vector3(-1191.14, -897.66, 13.35)},
+    },
+    closedShops = {
+        {ped = 'Nc_Pepe_CS', loc = vector4(1113.9280, -643.7979, 55.8214, 233.5980), label = 'Frog Cafe'}
+    },
+    closedShopItems = {
+        bzzz_sugary_macaron_a = {name = 'Macaron', price = 250},
+        bzzz_sugary_macaron_b = {name = 'Macaron', price = 250},
+        bzzz_sugary_cake_b = {name = 'Strawberry cake', price = 250},
+        bzzz_sugary_tiramisu_a = {name = 'Tiramisu', price = 250},
+        bzzz_sugary_mochi_a = {name = 'Lilly Mochi', price = 250},
+        bzzz_uwuprops_drink_a = {name = 'Frosty Iced Coffee', price = 100},
+        bzzz_uwuprops_drink_b = {name = 'Lillypad Iced Coffee', price = 100},
+        bzzz_uwuprops_drink_c = {name = 'Princess Iced Coffee', price = 100},
+        bzzz_uwuprops_drink_d = {name = 'Definitely Not Bloody Iced Coffee', price = 100},
+        bzzz_uwuprops_coffee_a = {name = 'Cute Coffee', price = 100},
+        bzzz_uwuprops_cappuccino_a = {name = 'Cute Cappuccino', price = 100},
+        bzzz_uwuprops_donut_a = {name = 'Sweet Donut', price = 250},
+    },
+    craftingStations = {
+        soda = {        
+            { anim = 'uncuff', give = {}, take = {bzzz_uwuprops_drink_a = 1}, progtext = 'Mixing' },
+            { anim = 'uncuff', give = {}, take = {bzzz_uwuprops_drink_b = 1}, progtext = 'Mixing' },
+            { anim = 'uncuff', give = {}, take = {bzzz_uwuprops_drink_c = 1}, progtext = 'Mixing' },
+            { anim = 'uncuff', give = {}, take = {bzzz_uwuprops_drink_d = 1}, progtext = 'Mixing' },
+        },
+        coffee = {
+            { anim = 'uncuff', give = {},    take = { bzzz_uwuprops_coffee_a = 1 }, progtext = 'Brewing' },
+            { anim = 'uncuff', give = {},    take = { bzzz_uwuprops_cappuccino_a = 1 }, progtext = 'Brewing' },
+        },
+        fryer = { -- donuts mainly       
+            { anim = 'uncuff', give = {flour = 1, milk = 1, egg = 1},  take = { bzzz_uwuprops_donut_a = 1 }, progtext = 'Cooking' },
+        },
+        ingredients = { -- ingredients
+            { anim = 'uncuff', give = {}, take = { flour = 1 }, progtext = 'Chopping' },
+            { anim = 'uncuff', give = {}, take = { milk = 1 },  progtext = 'Chopping' },
+            { anim = 'uncuff', give = {}, take = { egg = 1 },  progtext = 'Chopping' }
+        },
+        grill = {
+        },
+        bake = { -- bake
+            { anim = 'uncuff', give = { flour = 1, milk = 1, egg = 1 }, take = { bzzz_sugary_macaron_a = 1 }, progtext = 'Using' },
+            { anim = 'uncuff', give = { flour = 1, milk = 1, egg = 1 }, take = { bzzz_sugary_macaron_b = 1 }, progtext = 'Using' },
+            { anim = 'uncuff', give = { flour = 1, milk = 1, egg = 1 }, take = { bzzz_sugary_cake_b = 1 }, progtext = 'Using' },
+            { anim = 'uncuff', give = { flour = 1, milk = 1, egg = 1 }, take = { bzzz_sugary_tiramisu_a = 1 }, progtext = 'Using' },
+            { anim = 'uncuff', give = { flour = 1, milk = 1, egg = 1 }, take = { bzzz_sugary_mochi_a = 1 }, progtext = 'Using' },
+        }            
+    },
+    catering = {
+            commission = 0.2,
+            items = {
+            },
+            Van = {
+                vu = {model = 'burrito', label = 'Burrito', plate = 'TEQCATER', loc = vector4(-563.02, 287.51, 82.39, 180.0), heading = 28.78},
+            }
+
+    },
+    shops = {
+    },
+    locations = {
+        Crafter = {
+            {CraftData = {type = 'soda', targetLabel = 'Pour Drinks', menuLabel = 'Pour Drinks'},
+                loc = vector3(1113.8218, -640.8748, 56.9915), l = 0.65, w = 0.5, lwr = 0.25, upr = 0.25, r = 180, job = jobloc},
+            {CraftData = { type = 'coffee', targetLabel = 'Pour Coffee', menuLabel = 'Pour Coffee'},
+                loc = vector3(1115.7621, -640.3949, 57.0113), l = 0.7, w = 0.6, lwr = 0.25, upr = 0.25, r = 180, job = jobloc},
+            {CraftData = { type = 'fryer', targetLabel = 'Fry', menuLabel = 'Fry'},
+                loc = vector3(1116.4873, -634.1001, 56.8131), l = 0.75, w = 0.5, lwr = 0.5, upr = 0.5, r = 180, job = jobloc},
+            --{CraftData = { type = 'grill', targetLabel = 'Grill', menuLabel = 'Grill'},
+            --    loc = vector3(-1195.3722, -897.3842, 13.91), l = 2.0, w = 0.7, lwr = 0.5, upr = 0.5, r = 180, job = jobloc},
+            {CraftData = { type = 'ingredients', targetLabel = 'Grab Ingredients', menuLabel = 'Grab Ingredients'},
+                loc = vector3(1118.8975, -638.6453, 56.9960), l = 2.5, w = 2.5, lwr = 0.10, upr = 0.25, r = 180, job = jobloc},
+            {CraftData = { type = 'bake', targetLabel = 'Bake Goods', menuLabel = 'Bake Goods'},
+                loc = vector3(1114.4725, -634.5179, 56.7670), l = 2.7, w = 1.4, lwr = 0.5, upr = 0.5, r = 180, job = jobloc},
+        },
+        Stores = {
+            --{StoreData = {type = 'fridge', targetLabel = 'Fridge', menuLabel = 'Fridge'},
+            --    loc = vector3(-1196.06, -897.27, 14.20), l = 0.7, w = 0.5, lwr = 0.75, upr = 1.25, r = 180, job = jobloc},
+            --{StoreData = {type = 'ingridients', targetLabel = 'Ingridients', menuLabel = 'Ingridients'},
+            --    loc = vector3(-1196.06, -897.27, 14.20), l = 0.7, w = 0.5, lwr = 0.75, upr = 1.25, r = 180, job = jobloc},
+        },
+       Tills = {
+            { loc = vector3(1116.8439, -642.3646, 56.8509), l = 0.5, w = 0.5, lwr = 0.10, upr = 0.25, r = 168.27, commission = 0.8, job = jobloc },
+        },
+        trays = { -- storages to place things for people
+            {label = 'Grab Food',loc = vec3(1116.0913, -642.5754, 56.8953), l = 0.5, w = 0.5, lwr = 0.10, upr = 0.25, r = 180, slots = 6, weight = 30000, job = jobloc},
+        },
+        stash = { -- storages to place things
+            {label = 'Store Products', loc = vector3(1115.1011, -642.7894, 56.8111), l = 0.5, w = 0.5, lwr = 0.10, upr = 0.25, r = 180, slots = 100, weight = 600000, job = jobloc},  
+        },
+ 
+    },
+    consumables = {
+    },
+}
